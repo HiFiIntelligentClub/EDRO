68aa6787bc536e17e8f129d74d07d2a216a84763
Andrey Chekmarev <assminog@gmail.com>
2020-12-07 17:45:56 +0300

    Add files via upload

---
+++ 4.Objects/Write/Listener/Display/Element/Listener/Listener.php
@@ -0,0 +1,28 @@
+<?php
+/*© A.A.CheckMaRev assminog@gmail.com tubmulur@yandex.ru*/
+////// 
+   //   /\ RCe
+  //  <  **> 
+ //     Jl   
+//////
+class Listener
+	{
+	public $strHTML;
+	public function __construct($_objKIIM, $_arrData)
+		{
+		$objKIIM=$_objKIIM;unset($_objKIIM);
+		$objKIIM=KIIM::objStart($objKIIM, array('_strClass'=>__CLASS__, '_strMethod'=>__FUNCTION__, '_strMessage'=>''));
+
+		$this->strHTML='
+
+			';
+
+		KIIM::objFinish($objKIIM, array('_strClass'=>__CLASS__, '_strMethod'=>__FUNCTION__, '_strMessage'=>''));
+		}
+	public static function strHTML($_objKIIM, $_arrData)
+		{
+		$objListener=new Listener($_objKIIM, $_arrData);
+		return $objListener->strHTML;
+		}
+	}
+?>
