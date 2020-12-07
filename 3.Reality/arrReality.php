623ff565c30282f221fd257d58f6326b9b9dc456
Andrey Chekmarev <assminog@gmail.com>
2020-12-07 17:26:38 +0300

    Оптимизация

---
+++ 3.Reality/arrReality.php
@@ -0,0 +1,13 @@
+<?php
+/*© A.A.CheckMaRev assminog@gmail.com tubmulur@yandex.ru*/
+$arrReality=array(
+	'arrDirector'=>array(
+		),
+	'arrDj'=>array(
+		),
+	'arrGuitarist'=>array(
+		),
+	'arrDesigner'=>array(
+		),
+);
+?>
