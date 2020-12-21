952c30e03e0648baa847265cfe9a1dcba1c53499
Andrey Chekmarev <assminog@gmail.com>
2020-12-21 05:11:07 +0300

    Updated pages

--- 4.Objects/Read/Cloud/Disk/Pages/_UpdateMessage.php
+++ 4.Objects/Read/Cloud/Disk/Pages/_UpdateMessage.php
@@ -1,37 +1,148 @@
 <?php
 ?>
 <HficNews 
-	class="rel block TC1 BC1 BRJ BLL BBV BTA" 
+	class="rel block TC1 BC1" 
 	style="
 		height		:20px;
-		font-size	:small
+		font-size	:small;
+		line-height	:9px;
 		">
-
-
-	<ifEN><marquee scrollamount="2">U Excause me for deleting from list. This was a robots bad deal with some data corruption. Updating. Robots are stopped for a while. Hello. My name is Hfic Samin [Eichfik Samin]=[Hf->ik=HF &keys S=integral am=myself->in=+ =Samplus=Samples]. To enclosure in to the messages, press and hold CTRL button and rotate mouse wheel. Design will be adopted to your eyes needs. We are lookin for designer helper.</marquee></ifEN>
-	<ifRU><marquee scrollamount="2">Извиняюсь, что вас удалили с HIC, виноваты глюканувшие роботы. Скоро их исправим и вернём вас. Обновляемся. Роботы остановлены. Привет. Меня зовут Hfic Samin [Эйчфик Самин] (Эйч=H= google:stdio.h, , фик=фига=нет, Самин = САМ= язык СИ и АССЕМБЛЕР а не СИ++. Вот такой был смысл. Эйчфик Самин = АССемблер. Это когда САМ пишешь библиотеки для C++(.h)) Чтобы приблизить сообщения, зажмите кнопку Ctrl на клавиатуре и крутите колесо мыши. Дизайн сам подстроится под ваш выбор. Мы ищем помошника дизайнера.</marquee></ifRU>
-	<image
-		title		="Hfic Samin: Is there any hifi sound can i hear?" 
-		src		="/Hfic_Samin.jpg" 
-		class		="abs" 
+	<HIC_PRESIDENT_MESSAGE
+		class	="block" 
 		style		="
 				height		:100%;
-				top		:0;
-				right		:0px;
 				"
-		/>
-	<on-air 
-		title	="Active"
-		class	="abs block" 
-		style	="
-			top			:0;
-			right			:0;
-			width			:4px;
-			height			:4px;
-			background-color	:green;
-			"
 		>
-	</on-air>
+
+		<on-air 
+			title	="Active"
+			class	="abs block" 
+			style	="
+				top			:0;
+				right			:0;
+				width			:4px;
+				height			:4px;
+				background-color	:green;
+				"
+			>
+		</on-air>
+
+			<HficSamin style="color:green;font-size:xx-small;height:20px;width:100%" class="brick left ScrollerY"  mail-me-gmail="tubmulur@yandex.ru" message-me-telegram="https://t.me/hficsamin" message-me-whatsapp="+79117874457" view-oficial-group-main="https://vk.com/HiFiIntelligentClub" view-oficial-group-shine="https://facebook.com/HiFiIntelligentClub" title="https://t.me/hficsamin,whatsapp +79117874457,tubmulur@yandex.ru,https://vk.com/HiFiIntelligentClub,https://facebook.com/HiFiIntelligentClub">
+				<marquee 
+					class="brick left" 
+					style="width:15px;height:20px;color:red;font-size: x-large;margin-top:3px" 
+					scrollamount="5"
+					>
+					`''H''---....,,,,....''''`
+				</marquee>
+				<marquee 
+					class="brick left" 
+					style="width:15px;height:20px;color:blue;font-size: x-large;margin-top:5px" 
+					scrollamount="5"
+					>
+					``''''-E--....,,,,....''''``
+				</marquee> 
+				<marquee 
+					class="brick left" 
+					style="width:15px;height:20px;color:blue;font-size: x-large;margin-top:5px" 
+					scrollamount="5"
+					>
+					`''''---..L..,,,,....''''``
+				</marquee> 
+				<marquee 
+					class="brick left" 
+					style="width:15px;height:30px;color:red;font-size: x-large;margin-top:3px" 
+					scrollamount="5"
+					>
+					`''''---....,,L,,....''''
+				</marquee>
+				<marquee 
+					class="brick left" 
+					style="width:15px;height:30px;color:red;font-size: x-large;margin-top:3px" 
+					scrollamount="5"
+					>
+					`''''--....,,,,..O..''''`
+				</marquee>
+				<marquee 
+					class="brick left" 
+					style="width:15px;height:20px;color:blue;font-size: x-large;margin-top:5px" 
+					scrollamount="5"
+					>
+					``''''---....,,,,.:)...''''``
+				</marquee> 
+				<marquee 
+					class="brick left" 
+					style="width:15px;height:20px;color:blue;font-size: x-large;margin-top:5px" 
+					scrollamount="5"
+					>
+					`''''---...,,,,....''''``
+				</marquee> 
+				<marquee 
+					class="brick left" 
+					style="width:15px;height:30px;color:red;font-size: x-large;margin-top:3px" 
+					scrollamount="5"
+					>
+					`''''---....,,,....''''
+				</marquee>
+				<marquee 
+					class="brick left" 
+					style="width:15px;height:20px;color:blue;font-size: x-large;margin-top:5px" 
+					scrollamount="5"
+					>
+					`''''---...,,,,,,,....''''``
+				</marquee> 
+				<line class="block TC1 BC1 " style="height:20px;">
+					<ifEN>2020dec21[03:57:05]We remember our promises, and trying to implement them all.</ifEN>
+					<ifRu>2020dec21[03:57:05]Мы помним наши обещания, стараемся внедрить все.</ifRU>
+				</line>
+				<line class="block TC3 BC3 " style="height:10px;">
+					<ifEN>2020dec21[03:53:59]One more difficult step behind.Yeah!</ifEN>
+					<ifRu>2020dec21[03:53:59]Ещё один сложный шаг позади.Да!</ifRU>
+				</line>
+				<line class="block TC3 BC3 " style="height:10px;">
+					<ifEN>2020dec20[22:04:57]Pre testing on onion site new stuff</ifEN>
+					<ifRu>2020dec20[22:04:57]Тестирую на onion</ifRU>
+				</line>
+				<line class="block TC3 BC3 " style="height:10px; ">
+					<ifEN>2020dec17[07:41:30]Test on onion</ifEN>
+					<ifRu>2020dec17[07:41:30]Тестирую на onion</ifRU>
+				</line>
+				<line class="block TC3 BC3 " style="height:10px">
+					<ifEN>2020dec17[07:38:00]Hfic 75% cmplt;</ifEN>
+					<ifRu>2020dec17[07:38:00]Hfic 75% готово</ifRU>
+				</line>
+				<line class="block TC3 BC3 " style="height:10px;top:20px;">
+					<ifEN>2020dec12.06:25:00 Hfic return to update. Thnk you. Great Work!</ifEN>
+					<ifRu>2020dec12.06:25:00 Hfic возвращаюсь к апдейту. Great Work!</ifRU>
+				</line>
+				<line class="block TC3 BC3 " style="height:10px;top:30px;">
+					<ifEN>2020dec12.06:15:00 Hfic listening Wave Anime Radio!</ifEN>
+					<ifRu>2020дек12.06:15:00 Hfic слушает Wave Anime Radio!</ifRU>
+				</line>
+				<line class="block TC3 BC3 " style="height:10px;top:40px;">
+					<ifEN>Finished listening Dance Wave!</ifEN>
+					<ifRu>Закончил слушать Dance Wave!</ifRU>
+				</line>
+				<line class="block TC3 BC3 " style="height:10p;top:50px;">
+					<ifEN>Finished listening TRANCE.one</ifEN>
+					<ifRu>Закончил слушать Dance Wave!</ifRU>
+				</line>
+				<line class="block TC3 BC3 " style="height:10px;top:60px;">
+					<ifEN>finished PulsRadio TRANCE</ifEN>
+					<ifRu>Закончил слушать PulsRadio TRANCE</ifRU>
+				</line>
+				<line class="brick TC3 BC3 " style="height:10px;top:70px;">
+					<ifEN>Finished Pleasure Beats by John Macraven</ifEN>
+					<ifRu>Закончил слушать Pleasure Beats by John Macraven</ifRU>
+				</line>
+
+	    		</HficSamin>
+
+			<!--marquee scrollamount="2" class="brick" style="width:50%;Height:20px;">
+				13dec2020_00:06 Please note, HIC is INTELLIGENT. Our smart robots are gentle. Because they are smart. Silly robots from MashroomAviation(the core of HIC was financial robot, rebuilded after forbid crypto exchange by government). Silly financial robot was moneymaking robot. It is the core. So if you see, some silly thing in smart gentel intelligent, please feed me tubmulur@yandex.ru. I am at communication pause to create HIC objects for harmonic and complete, easy to use for me, and, I think in a short perspective, for all dj's update. 12DEC2020 23:52 You can see ON-AIR brick. It is pink now. Will be integrated soon. Your status: are you now in te play or this is record. Inspired by <a href="http://hifiintelligentclub.com/?strName=Pleasure%20Beats%20by%20John%20Macraven&strStyle=&intBitrate=&strCodec=&int0Page=3&int1OnPage=2&int0PlayingStationNum=0&strPlayingStationStyle=&strPlayingStationId=XBUkAnNcbiUOARV%25D0%25BE49%25D0%25BEG0QNMQs7EiUvAFwPNQYOWGVCeVwgMxsdBTo%25D0%25BE26%25D0%25BE#">John Macraven [Respect link]</a> . Thank you Mr. Macraven. You will not be forgived by the smart HIC robots.....   14:53 testing for bad connection. Sorry.  2020-dec-12 11:06 forming pacvkages. Will no go home before it will done. 20-dec-07 06:29:27. ICQR will test stations very slow 2020-dec-10 14:15:27
+			</marquee-->
+		<ifRU><marquee class="brick left" style="width:5px;color:blue;font-size: xx-large;" scrollamount="10">``''''---....,,,,....''''``</marquee><HficSamin style="color:green;font-size:xx-small;" class="brick left"  mail-me-gmail="tubmulur@yandex.ru" message-me-telegram="https://t.me/hficsamin" message-me-whatsapp="+79117874457" view-oficial-group-main="https://vk.com/HiFiIntelligentClub" view-oficial-group-shine="https://facebook.com/HiFiIntelligentClub" title="https://t.me/hficsamin,whatsapp +79117874457,tubmulur@yandex.ru,https://vk.com/HiFiIntelligentClub,https://facebook.com/HiFiIntelligentClub">HFIC ICQR ms</HficSamin><marquee class="brick left" style="width:5px;color:red;font-size: xx-large;" scrollamount="10">``''''---....,,,,....''''``</marquee> <marquee scrollamount="2" class="brick left" style="width:10%;">Russian translate time i have, to mutch updates] You can see ON-AIR brick. It is pink now. Will be integrated soon. Your status: are you now in te play or this is record. Inspired by John Macraven. Thank you Mr. Macraven. You will not be forgived by the smart HIC robots.....   1 14:54 Тестируем для плохого соединения. Приносим извинения за статистический коллапс. 12 декабря 2020 11:09 +++формируем пакеты распределенных вычислений5%+++, реальности БД, реакцию на ЕДРО:ПОЛИМЕР.ЕСЛИ, объект интерфейса LINE cookie, cookie policy, player 2 dj events,trace path for web 2.0 elements, вырастет в несколько сотен раз нагрузка на БД,  для поддержания актуальных событий, миграция ДБ ЕДРО:ПОЛИМЕР -< ЕДРО:ПОЛИМЕР.о.2, проверка поиска по тегам с &, - итд, турецкие, фарси, нохчийн мотт, персидские и арабские станции, китайские станции. Мы сегодня не уйдём домой, пока не сформируем и не устанвим сделанные дополнение.Спасибо огромное диджеям, без хорошей музыки работа бы не продвигалась вобще. Спасибо! Исходные коды: https://github.com/hifiintelligentclub.com. H:Большой апдейт проворачивает мои мозги. Но это того стоит, Сами увидите :) Эйчфик.Самин.07-дек-2020 06:25:40. ICQR выборочно протестирует станции сегодня. Это будет мало станций, отобранных на вкус АйСиКваЕРа.7-дек-2020</marquee></ifRU>
+	</HIC_PRESIDENT_MESSAGE>
 </HficNews>
 <?php
 ?>
