108a6fdf329a343870570719f5b89959cd71b576
Andrey Chekmarev <assminog@gmail.com>
2021-02-04 04:24:55 +0300

    fix

--- 3.Reality/Reality.php
+++ 3.Reality/Reality.php
@@ -23,7 +23,8 @@ Site[En] Private browsing international: http://ryklzxobxv4s32omimbu7d7t3cdw6dpl
 ./././././././*/
 class Reality extends Objects
 	{
-	protected $strBasePath	='/home/EDRO';
+	protected 	$strBasePath	='/home/EDRO';
+	public 		$arrReality	=array();
 	public function __construct($_objKIIM)
 		{$objKIIM=$_objKIIM;unset($_objKIIM);$objKIIM=KIIM::objStart($objKIIM, array('_strClass'=>__CLASS__, '_strMethod'=>__FUNCTION__, '_strMessage'=>''));
 	///////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -32,12 +33,12 @@ class Reality extends Objects
 
 	///////////////////////////////////////////!!!!
 		
-		
+		$this->arrReality['сРасположениеКорень']	='/home/ЕДРО:ПОЛИМЕР/о2о.БазаДанных/HiFiIntelligentClub';
 		$this->arrReality['strRoleSignal']		='Listener';
 		$this->arrReality['strListnersPath']		='/home/EDRO.o2o/'.$this->arrReality['strRoleSignal'];
 		$this->arrReality['strListenerId']		=СоздатьСеанс::с($objKIIM, $this->arrReality['strRoleSignal'] , $this->arrEvent['arrReality']);
 		$this->arrReality['arrCurrentListeners']	=ПрочитатьСлушателей::м($objKIIM, $this->arrReality['strListnersPath']);
-
+		//exit;
 		/////////////////////////////////////////////////////////////////////////////////////////////////////////
 		
 		$this->arrReality['strRoleLangSignal']		=rmLb(FileRead::str($objKIIM, $this->strBasePath.'/3.Reality/User/'.$this->arrReality['strRoleSignal'].'/.strLang.php'));
@@ -45,6 +46,7 @@ class Reality extends Objects
 		$this->arrReality['bIzAndroid']			=$this->bIzAndroid();
 		$this->arrReality['bIzApple']			=$this->bIzApple();
 		$this->arrReality['bIzDesktop']			=$this->bIzDesktop();
+		
 		//print_r($this->arrReality);
 		//print_r($_SESSION);
 		//print_r($this->arrReality);
@@ -105,11 +107,13 @@ class Reality extends Objects
 	private function bIzDesktop()
 		{
 		$bIz=false;
-		if(!$this->bIzAndroid()&&!$this->bIzApple())
+		if($this->bIzAndroid()===false)
 			{
-			$bIz=true;
+			if($this->bIzApple()===false)
+				{
+				$bIz=true;
+				}
 			}
-
 		return $bIz;
 		}
 	private function _IsDNT()
@@ -122,8 +126,17 @@ class Reality extends Objects
 		}
 	public static function strObjectInit()
 		{	
-		return EDRO::strObjInit('Reality');
+		$str	=EDRO::strObjInit('Reality');
+		$str	.="
+			<script>
+				console.log('[.++.+.*.*.++.+.+]EDRO.objReality.O: Init event stream graph circle.');
+				objReality._CircleControllerGraph	=setInterval(objReality.funControllerGraph, 1000);
+				console.log('[......*.*.......]EDRO.objReality.O: Init event stream graph circle.');
+			</script>
+			";
+		return $str;
 		}
+
 	public static function strObjectDeclare()
 		{
 		$str	=<<<oo2oo
@@ -136,6 +149,7 @@ class Reality extends Objects
 					{
 					console.log('[Vv]EDRO.Reality: Construct');
 					//this.objDebugString		=document.getElementById('strPlayerPlayEventsDebugString');
+					this.strLanguage		='';
 					this.intStep			=0;
 					this.intVector			=0;
 					this.intStep2News		=0;
@@ -183,12 +197,138 @@ class Reality extends Objects
 					/*		1  1 1 1=1 десктоп    					*/
 					/*		0  0 0 0						*/
 					/*									*/
-					if((this.deviceType.indexOf("android")==-1)&&(this.deviceType.indexOf("iphone")==-1 || this.deviceType.indexOf("ipad")==-1|| this.deviceType.indexOf("ipod")==-1))
+					if(
+					(this.deviceType.indexOf("android")==-1)&&
+					(this.deviceType.indexOf("iphone")==-1 || 
+					this.deviceType.indexOf("ipad")==-1|| 
+					this.deviceType.indexOf("ipod")==-1)
+					)
 						{
 						//this.objDebugString.innerHTML='Hfic.Samin+!!Desktop test!! all is ok';  //temp
 						return true;
 						}
 					return false;
+					}
+				funControllerGraph()
+					{
+					console.log('[Vv]EDRO.O: Declare');
+					var objDate	=new Date();
+					if(objPlayer.bIzPlaying) //!!!    objReality.bIzPlayerPlaying!!!
+						{
+						document.title	=objPlayer.strStationName+' at HiFiIntelligentClub.com';
+						//objPlayer.objVisibleControlsPlayingDuration.innerHTML=objPlayer.intPlayingDuration;
+						//objPlayer.objVisibleControlsPlayingDuration.innerHtml	=objPlayer.intPlayingDuration;
+						objPlayer.intPlayingDuration++;
+						}
+					if(objReality.arrPlayer.bIzLoading||objReality.arrPlayer.bIzPlaying)
+						{
+					//	console.log(objPlayer.objAudio.textTracks);
+						if(objPlayer.objAudio.networkState==1)
+							{
+							objIndicatorNetwork.objStrSrvReady.style.backgroundColor	='yellow';
+							objIndicatorNetwork.objStrSrvReady.style.color			='black';
+							}
+						else if(objPlayer.objAudio.networkState==2)
+							{
+							objIndicatorNetwork.objStrSrvReady.style.backgroundColor	='green';
+							objIndicatorNetwork.objStrSrvReady.style.color			='white';
+							}
+						else
+							{
+							objIndicatorNetwork.objStrSrvReady.style.backgroundColor	='red';
+							objIndicatorNetwork.objStrSrvReady.style.color			='white';
+							}
+						if(objPlayer.objAudio.readyState==0)
+							{
+							objIndicatorNetwork.objStrSrvPortsReady.style.backgroundColor	='red';
+							objIndicatorNetwork.objStrSrvPortsReady.style.color		='white';
+							}
+						else if(objPlayer.objAudio.readyState==1)
+							{
+							objIndicatorNetwork.objStrSrvPortsReady.style.backgroundColor	='yellow';
+							objIndicatorNetwork.objStrSrvPortsReady.style.color		='black';
+							}
+						else if(objPlayer.objAudio.readyState==2)
+							{
+							objIndicatorNetwork.objStrSrvPortsReady.style.backgroundColor	='blue';
+							objIndicatorNetwork.objStrSrvPortsReady.style.color		='white';
+							}
+						else if(objPlayer.objAudio.readyState==3)
+							{
+							objIndicatorNetwork.objStrSrvPortsReady.style.backgroundColor	='green';
+							objIndicatorNetwork.objStrSrvPortsReady.style.color		='white';
+	    						}
+						else
+							{
+							objIndicatorNetwork.objStrSrvPortsReady.style.backgroundColor	='white';
+							objIndicatorNetwork.objStrSrvPortsReady.style.color		='black';
+							}
+						objPlayer.intPlayingDuration++;
+						}
+					if(objReality.arrPlayer.bIzLoading)
+						{
+						//objPlayer.objVisibleControlsLoadingDuration.innerHTML=objPlayer.intLoadingDuration;
+						if(objPlayer.intPlayerLoadingAnim==3)
+							{
+							objPlayer.intPlayerLoadingAnim				=0;
+							objPlayer.strPlayerLoadingAnim				='';
+							}
+						objPlayer.intPlayerLoadingAnim++;
+						objPlayer.strPlayerLoadingAnim				+='.';
+						document.title						=objPlayer.strPlayerLoadingAnim+decodeURIComponent(objPlayer.strStationName)+'on HiFiIntelligentClub.com';
+						objIndicatorMasterClock.objStrServerLoading.innerHTML	=objReality.intMaxLoadingTime+'/'+objReality.intLoadingTime+'/'+objPlayer.intLoadingDuration;
+						if(!objPlayer.bIzNeedToBeStoppedEvent)
+							{
+							//objPlayer.objAudio.load();
+							}
+						objPlayer.intLoadingDuration++;
+						//objPlayer.bIzLoading=false;
+						}
+					else
+						{
+						objPlayer.strPlayerLoadingAnim	='';
+						document.title	=objPlayer.strPlayerLoadingAnim+decodeURIComponent(objPlayer.strStationName)+'|HiFiIntelligentClub.com';
+						}
+					if(objReality.bIzLoading)
+						{
+						objIndicatorMasterClock.objStrServerLoading.innerHTML	=objReality.intMaxLoadingTime+'/'+objReality.intLoadingTime;
+						if(objReality.intLoadingTime>=objReality.intMaxLoadingTime)
+							{
+							objReality.intMaxLoadingTime		=objReality.intLoadingTime;
+							}
+						objReality.intLoadingTime++;
+						}
+					//if(objPlayer.bIzError)
+					//	{
+				
+					//	}
+					if(objReality.arrPlayer.bIzWaiting)
+						{
+					
+						}
+					if(objReality.intVector==2)
+						{
+						objReality.intVector	=0;
+						if(bizHiFiNavigationInputSelect) //In Dynascreen input focus, no template transitions, wait for human input
+							{
+							}
+						else
+							{
+							//objIndicatorLang._UpdateIndicator();
+							objDesign._UpdateDimensions();
+							objDesign._CheckElements();
+							}
+						}
+					if(objReality.intStep2News==60)
+						{
+						objReality.intStep2News	=0;
+						}
+					objIndicatorMasterClock.objStr.innerHTML	=objReality.intStep++;
+					objReality.intVector++;
+					objReality.intStep2News++;
+					objEDRO._CircleControllerGraph;
+					console.log('[..]EDRO.O: Declare');
+					return 0;
 					}
 				}
 		console.log('[.]EDRO.Reality: Declare');
