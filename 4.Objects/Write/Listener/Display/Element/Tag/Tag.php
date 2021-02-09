75a7b77b9f005e1cd6b58b28d0c63a9b1b4e0b72
Andrey Chekmarev <assminog@gmail.com>
2021-02-09 12:35:12 +0300

    EDRO

--- 4.Objects/Write/Listener/Display/Element/Tag/Tag.php
+++ 4.Objects/Write/Listener/Display/Element/Tag/Tag.php
@@ -12,36 +12,41 @@ class Tag
 	private		$strParamName;
 	private		$arrParams;
 	private 	$strLayer;
-	public function __construct($_objKIIM, $_strTag, $arrParams, $_strParamName='strTag', $_intLayer='11')
+	public function __construct($_objKIIM, $_strStyle, $arrParams, $_strParamName='strStyle', $_intLayer='11', $objEDRO=array())
 		{
 		$this->arrParams	=$arrParams;
 		$this->intLayer		=$_intLayer;
 		$this->strParamName	=$_strParamName;
-		$this->strReality	=$_strTag;
-		$this->strHTML		.=$this->strObject();
-		//echo $_strTag;
-		//if(!empty($мФразы)&&is_array($мФразы))
-		//	{
-		//	$мФразы			=мСобратьФразы($_strTag);
-		//	foreach($мФразы as $сФраза)
-		//		{
-		//		$this->strReality	=$сФраза;
-		//		//echo '<br/>';
-		//		$this->strHTML		.=$this->strObject();
-		///		}
-		//	}
+		$this->strReality	=$_strStyle;
+		$this->strLanguage	=$objEDRO->arrReality['strLangSignal'];
+		$strOpen="<";
+		$strClose=">";
+
+		$str=$strOpen.'a '."\n";
+				$str.=$this->strEvent()."\n";
+				$str.=$this->strDesign()."\n";
+				$str.=$this->strObject()."\n";
+			$str.="\t".$strClose;
+			$str.="\t".$this->strReality()."\n";
+		$str.=$strOpen.'/'.'a'.$strClose."\n";
+		$this->strHTML		.=$str;
 /*-[.]*/	}
 /*-[E]*/private function strEvent()
 		{
 		//echo 'strEvent';
 		//echo '<br/>';
+		$strE	='';
 		if(сДляСравнения($this->arrParams[$this->strParamName])==сДляСравнения($this->strReality))
 			{
-			$strE	=strEventLink(arrEventLink($this->arrParams, $this->strParamName, '', 		false, 0));
+			$arrEventLink	=arrEventLink($this->arrParams, $this->strParamName, '', 		false, 0);
+			$strE	.=$arrEventLink['strHref'];
+			$strE	.=$arrEventLink['strOnClick'];
 			}
 		else
 			{
-			$strE	=strEventLink(arrEventLink($this->arrParams, $this->strParamName, $this->strReality(), false, 0));
+			$arrEventLink	=arrEventLink($this->arrParams, $this->strParamName, $this->strReality(), false, 0);
+			$strE	.=$arrEventLink['strHref'];
+			$strE	.=$arrEventLink['strOnClick'];
 			}
 		return $strE;
 /*-[.]*/	}
@@ -50,25 +55,27 @@ class Tag
 		
 		if(сДляСравнения($this->arrParams[$this->strParamName])==сДляСравнения($this->strReality))
 			{
-			$strD	=' '.'class	="sensor block rel left BC2 TC2 BLL2 BTA2 layer_'.$this->intLayer.'"'.' line';
+			$strD	=' '.'class	="sensor block rel left BC2 TC2 BLL2 BTA2 layer_'.$this->intLayer.'"'.' ';
 			$strD	.=' '.'style	="
 				padding-left	:5px;
 				padding-right	:5px;
 				text-align	:center;
-				line-height	:18px;
+				font-size	:medium;
 				height		:19px;
+				line-height	:18px;
 				text-decoration	:none;
 				"'.' ';
 			}
 		else
 			{
-			$strD	=' '.'class	="sensor block rel left BRJ2 BBV layer_'.$this->intLayer.'"'.' line';
+			$strD	=' '.'class	="sensor block rel left BRJ2 BBV layer_'.$this->intLayer.'"'.' ';
 			$strD	.=' '.'style	="
 				color		:#4d4d4d;
 				background-color:#e1dfdf;
 				padding-left	:5px;
 				padding-right	:5px;
 				text-align	:center;
+				font-size	:medium;
 				height		:19px;
 				text-decoration	:none;
 				"'.' ';
@@ -83,19 +90,19 @@ class Tag
 		}
 /*-[O]*/private function strObject()
 		{
-		$strOpen="<";
-		$strClose=">";
-		$strO=$strOpen.'a ';
-				$strO.=$this->strEvent();
-				$strO.=$this->strDesign();
-			$strO.=$strClose;
-			$strO.=$this->strReality();
-		$strO.=$strOpen.'/'.'a'.$strClose;
+		if($this->strLanguage=='RU')
+			{
+			$strO='title="Нажмите для того чтобы передать поисковой машине сигнал, искать все станции с этим жанром"';
+			}
+		else
+			{
+			$strO='title="Press this button  to search all stations with this genre"';
+			}
 		return $strO;
 /*-[.]*/	}
-/*-[R]*/public static function strHTML($_objKIIM, $_strTag, $arrParams, $strParamName='strTag', $_intLayer='11')
+/*-[R]*/public static function strHTML($_objKIIM, $_strStyle, $arrParams, $strParamName='strStyle', $_intLayer='11', $objEDRO=array())
 		{
-		$obj=new Tag($_objKIIM, $_strTag, $arrParams, $strParamName, $_intLayer);
+		$obj=new Tag($_objKIIM, $_strStyle, $arrParams, $strParamName, $_intLayer);
 		return $obj->strHTML;
 /*-[.]*/	}
 	}
