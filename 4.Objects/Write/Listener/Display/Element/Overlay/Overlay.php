fac7397f216cef3832230b26795084f499fd1e11
Andrey Chekmarev <assminog@gmail.com>
2021-02-09 12:25:43 +0300

    EDRO

--- 4.Objects/Write/Listener/Display/Element/Overlay/Overlay.php
+++ 4.Objects/Write/Listener/Display/Element/Overlay/Overlay.php
@@ -26,17 +26,37 @@ class Overlay
 	public 	$strHTML;
 	private $arrEvent	=
 		array(
+		'close'=>
+			array(
 			'strLink'		=>'',
-			'strOnClick'		=>'',
+			'strOnClick'		=>'this.parentNode.className +=\' hidden\';',
+			)
 		);
 	private $arrDesign	=
 		array(
+		'close'=>
+			array(
+			'strClass'		=>'',
+			'strStyle'		=>'',
+			'intLayer'		=>'',
+			),
+		'informationWindow'=>
+			array(
+			'strClass'		=>'',
+			'strStyle'		=>'',
+			'strLayer'		=>'layer_6',
+			),
+		'scrollerPart '=>
+			array(
 			'strClass'		=>'',
 			'strStyle'		=>'',
 			'intLayer'		=>1,
+			),
 		);
 	private $objReality	=
 		array(
+		'close'=>
+			array(
 			'arrRole'=>
 			array(
 				'Listener', 'Operator'
@@ -46,77 +66,128 @@ class Overlay
 				'ifEN'	=>'',
 				'ifRU'	=>'',
 				),
-			);
-	private $arrObjects	=array(
-			'strClass'		=>'a',
-			'strOpen'		=>'>',
-			'strClose'		=>'<',
+			),
+		'informationWindow'=>
+			array(
+			'arrRole'=>
+			array(
+				'Listener', 'Operator'
+				),
+			'arrLang'=>
+			array(
+				'ifEN'	=>'',
+				'ifRU'	=>'',
+				),
+			),
+		'scrollerPart'=>
+			array(
+			'arrRole'=>
+			array(
+				'Listener', 'Operator'
+				),
+			'arrLang'=>
+			array(
+				'ifEN'	=>'',
+				'ifRU'	=>'',
+				),
+			),
 		);
-
-	public function __construct($_objKIIM, $_strStyle, $arrParams,  $_intLayer)
+	private $arrObjects	=
+		array(
+		'close',
+		'informationWindow',
+		'scrollerPart'
+		);
+	public function __construct($_objKIIM=array())
 		{
-		$this->arrEvent	=
-		array(
-			'strLink'		=>'"/search'.strLinkGroove($this->arrEvent['arrParams'], 'strStyle', '').'"',
-			'strOnClick'		=>
-<<<oo2oo
-					//objSearch.objValueInputStyle.value='';
-					objEvent.arrParams.strStyle='';
-					objEvent._UpdateURLDyn();
-					return false;
-oo2oo,
-			'arrParams'		=>$arrParams,
-			);
-		$this->arrDesign=
-		array(
-			'strClass'		=>'block abs layer_'.($_intLayer+1),
-			'strStyle'		=>
-<<<oo2oo
-					top		: 0;
-					left		: 0;
-					width		: 100%;
-					height		: 100%;
-					text-align	: center;
-					color		: #fff;
-					background-color: #000;
-oo2oo,
-			);
-		$this->objReality=
-			array(
-			'arrRole'=>
-				array(
-				'Listener', 'Operator'
-				),
-			'arrLang'=>
-			array(
-				'ifEN'	=>'',
-				'ifRU'	=>'',
-				),
-			);
-		$this->arrObjects=
-		array(
-			'strClass'		=>'a',
-			'strOpen'		=>'<',
-			'strClose'		=>'>',
-			);
+		$this->strHTML='
+		<informationWindow
+			id	="objInformationOverlay"
+			class	="fixed V94 block window '.$this->arrDesign['informationWindow']['strLayer'].' TC3 BC3 BBV BTA"
+			style	="
+				width		:100vw;
+				height		:300px;
+				max-height	:50vh;
+				"
+			>
+			<close
+				class		="block TC1 BC1 sensor"
+				onClick		="'.$this->arrEvent['close']['strOnClick'].'"
+				>
+				<close
+					class	="block right"
+					style	="
+						width		: 40px;
+						color		: #fff;
+						background-color: #000;
+						text-align	: center;
+						"
+						>
+						X
+	    				</close>
+			</close>
+			<scrollerPart 
+				class	="block scrollerY"
+				style	="
+					width		:100vw;
+					height		:240px;
+					max-height	:40vh;
+					"
+				>
+				<ifRU>
+					<textarea>
+						<h1>HiFiIntelligentClub.</h1>
+						<a name="УвеличитьЭкран"><h2>Слишком мелко?</h2></a>
+						<p>
+							<color style="font-size:x-large;color:green">Зажмите клавишу ctrl(Контрл) 
+							и крутите колесо мышки. Станет крупнее/мельче.</color><br/>
+							Приложение само подстроится под ваши настройки, удалив с экрана выступающие за край элементы.<br/>
+							CTRL+Колесо мыши: Выведите на экран станцию такого размера, как вам удобно смотреть!
+						</p>
+						<p>
+							Искренне ваш Hfic.Samin Президент HiFiIntelligentClub.
+						</p>
+					</p>
+					</textarea>
+				</ifRU>
+				<ifEN>
+					<textarea>
+					</textarea>
+					<p>
+						<h1>HiFiIntelligentClub.</h1>
+						<a name="Enlarge_ctrlmouseWheel"><h2>"Too small text?"</h2></a>
+						<p>
+							<color style="font-size:x-large;color:green">Hold down the ctrl key and turn the 
+							mouse wheel</color><br/>
+							The application will adjust itself to your vision, Displaying a station of the 
+							size that is convenient for you to look at!
+						</p>
+						<p>
+							Sincerely yours Hfic. Samin President of HiFiIntelligentClub.
+						</p>
+					
 
-		if(сДляСравнения($arrParams['strStyle'])==сДляСравнения($_strStyle))
-			{
-			$this->objReality['arrLang']['ifEN']	=$_strStyle;
-			$this->objReality['arrLang']['ifRU']	=$_strStyle;
-			$this->strHTML				=$this->strObject();
-			}
-		}
+				</ifEN>
+			<ifEN>NO COOKIES! COOKIES KILLS!</ifEN><ifRU>НЕТ КУКИЗ. КУКИ УБИВАЮТ!</ifRU>
+			</scrollerPart>
+
+		</informationWindow>';
+
+
+
+
+/*-[.]*/	}
+
 /*-[E]*/private function strEvent()
 		{
-		$strE='href="'.$this->arrEvent['strLink'].'" ';
-		$strE.='onClick="'.$this->arrEvent['strOnClick'].'" ';
-		return $strE;
+		//$strD='class="'.$this->arrDesign['strClass'].'" ';
+		//$strD.='style="'.$this->arrDesign['strStyle'].'" ';
+		return $strD;
 /*-[.]*/	}
 /*-[D]*/private function strDesign()
 		{
-		$strD='class="'.$this->arrDesign['strClass'].'" ';
-		$strD.='style="'.$this->arrDesign['strStyle'].'" ';
+		//$strD='class="'.$this->arrDesign['strClass'].'" ';
+		//$strD.='style="'.$this->arrDesign['strStyle'].'" ';
 		return $strD;
 /*-[.]*/	}
 /*-[R]*/private function strReality()
@@ -124,23 +195,17 @@ oo2oo,
 		//print_r($_SESSION);
 		//$this->objReality['arrRole'];
 		//$this->objReality['arrLang'];
-		$strR='<ifRU>'.$this->objReality['arrLang']['ifRU'].'</ifRU>';
-		$strR.='<ifEN>'.$this->objReality['arrLang']['ifEN'].'</ifEN>';
+		//$strR='<ifRU>'.$this->objReality['arrLang']['ifRU'].'</ifRU>';
+		//$strR.='<ifEN>'.$this->objReality['arrLang']['ifEN'].'</ifEN>';
 		return $strR;
 		}
 /*-[O]*/private function strObject()
 		{
-		$strE=$this->strEvent();
-		$strD=$this->strDesign();
-		$strR=$this->strReality();
-		$strO=$this->arrObjects['strOpen'].$this->arrObjects['strClass'].$this->arrObjects['strClose']."\n";
-		$strO.=$strE.$strD.$strR;
-		$strO.=$this->arrObjects['strOpen'].'/'.$this->arrObjects['strClass'].$this->arrObjects['strClose']."\n";
 		return $strO;
 /*-[.]*/	}
-	public static function strHTML($_objKIIM, $_strStyle, $arrParams, $_intLayer)
+	public static function strHTML($_objKIIM=array())
 		{
-		$objOverlay=new Overlay($_objKIIM, $_strStyle, $arrParams, $_intLayer);
+		$objOverlay=new Overlay($_objKIIM);
 		return $objOverlay->strHTML;
 		}
 	}
