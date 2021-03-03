a1d7e3b052243364892eb722538777e48f4ea34f
Andrey Chekmarev <assminog@gmail.com>
2021-03-04 00:09:57 +0300

    EDRO

--- 4.Objects/Objects.php
+++ 4.Objects/Objects.php
@@ -11,25 +11,24 @@ VK:       https://vk.com/Hfic.Samin
 VK:       https://vk.com/HiFiIntelligentClub
 Facebook: https://facebook.com/Hfic.Samin
 Facebook: https://facebook.com/HiFiIntelligentClub
-Site[Ru] Public browsing international:  http://HiFiIntelligentClub.Ru
-Site[En] Public browsing international:  http://HiFiIntelligentClub.COM
+Site[Ru] Public browsing international :  http://HiFiIntelligentClub.Ru
+Site[En] Public browsing international :  http://HiFiIntelligentClub.COM
 Site[En] Private browsing international: http://ryklzxobxv4s32omimbu7d7t3cdw6dplvsz36zsqqu7ad2foo5m3tmad.onion
-            
-|E    |D     |R      |O      |											
-|Event|Design|Reality|Objects|										
+
+|E    |D     |R      |O      |
+|Event|Design|Reality|Objects|
 |ЕСЛИ /ДЕЙСТВИЕ/РЕАЛЬНОСТЬ/ОБЪЕКТ/
 
-
-//        /\              
-//      <  **>           
- //////   jl             
-./././././././*/							
+ ////
+//        /\
+//      <  **>
+ //////   jl
+./././././././*/
 
 class Objects
 	{
-	public function __construct($_objKIIM)
-		{$objKIIM=$_objKIIM;unset($_objKIIM);$objKIIM=KIIM::objStart($objKIIM, array('_strClass'=>__CLASS__,'_strMethod'=>__FUNCTION__, '_strMessage'=>'', '_objCurrent'=>'', '_intShowTempo'=>'' ));
-
+	public function __construct($мКИМ)
+		{
 		//$this->arrReality['сРасположениеКорень']	='/home/ЕДРО:ПОЛИМЕР/о2о.БазаДанных/HiFiIntelligentClub'; //moved to REALITY
 		$strPlatformPrefix	='';
 		$strHiFiType		=сПреобразовать($this->arrEvent['arrReality']['strHiFiType'], 'вСтроку');
@@ -40,7 +39,9 @@ class Objects
 		$arrHiFi['HiFi casual']		='casual'; //HiFi
 		$arrHiFi['HiFi often']		='often';  //HiFi
 		$arrHiFi['HiFi mustbe']		='mustbe'; //HiFi
-		//$arrHiFi[]='HiFi 2';       //2.1
+		$arrHiFi['HiFi modern']		='HiFi 2.1'; //2.1
+		$arrHiFi['HiFi modern_casual']	='HiFi 5.1'; //5.1
+		$arrHiFi['HiFi modern_casual+']	='HiFi 7.1'; //7.1
 		//print_r($this->arrEvent['arrReality']['strStationID']);
 		//exit();
 		foreach($arrHiFi as $strHiFiTypeName =>$strHiFiShortName)
@@ -92,22 +93,17 @@ class Objects
 			$strSearchPath	.='/Genres/search/'.$strSearchGenre.$strSearchType;
 			}
 		$strSearchPath;
-
-		
-
-		if($this->arrEvent['arrReality']['strStationID']!='')
+		if($this->arrEvent['arrReality']['strPlayingStationId']!='')
 			{
 			}
 		else
 			{
-			
 			}
-
 
 		$this->arrObjects['сРасположение']		=$this->arrReality['сРасположениеКорень'].$strSearchPath;
 		if($strSearchName=='')
 			{
-			$objTotal	=FileRead::objJSON($objKIIM, $this->arrObjects['сРасположение'].'/total.plmr');
+			$objTotal	=FileRead::objJSON($this->arrObjects['сРасположение'].'/total.plmr');
 
 			$this->arrObjects['ч0РасположениеTotal']	= ($objTotal->int1Total-1);
 			if($this->arrObjects['ч0РасположениеTotal']=='')
@@ -115,13 +111,12 @@ class Objects
 				echo 'No data';
 				}
 		//exit;
-			$this->arrObjects['мРасположение']		=Pagination::arr($objKIIM, $this);
+			$this->arrObjects['мРасположение']		=Pagination::arr($this);
 
 			for($int0I=$this->arrObjects['мРасположение']['int0Start'];$int0I<=$this->arrObjects['мРасположение']['int0Untill'];$int0I++)
 				{
 				$this->arrObjects['мТаблица'][]	=$this->arrObjects['сРасположение'].'/'.$int0I.'.plmr';
 				}
-
 			}
 		else
 			{
@@ -161,7 +156,7 @@ class Objects
 				$ч0РасположениеTotal			=0;
 				}
 			$this->arrObjects['ч0РасположениеTotal']	=$ч0РасположениеTotal;
-			$this->arrObjects['мРасположение']		=Pagination::arr($objKIIM, $this);
+			$this->arrObjects['мРасположение']		=Pagination::arr($this);
 
 			for($int0I=$this->arrObjects['мРасположение']['int0Start'];$int0I<=$this->arrObjects['мРасположение']['int0Untill'];$int0I++)
 				{
@@ -182,8 +177,6 @@ class Objects
 		//print_r($this);
 		//echo '</pre>';
 		//exit;
-
-		KIIM::objFinish($objKIIM, array('_strClass'=>__CLASS__, '_strMethod'=>__FUNCTION__, '_strMessage'=>''));
 		}
 	public static function strObjectInit()
 		{
@@ -204,16 +197,17 @@ class Objects
 				width		:100%;
 				"
 			>
-		<font style="font-size:xx-small">© HiFiIntelligentClub.COM tubmulur@yandex.ru 2020 
-			<ifRU 
-				
-				>
-				На облаке Reg.Ru.
-			</ifRU>
-			<ifEN
-				>
-				On Reg.Ru cloud.
-			</ifEN>
+			<font 
+				style="font-size:xx-small"
+				>HiFiIntelligentClub. © tubmulur@yandex.ru 2021 
+				<ifRU
+					>
+					На облаке Reg.Ru.
+				</ifRU>
+				<ifEN
+					>
+					On Reg.Ru cloud.
+				</ifEN>
 			</font>
 		</copyright>
 		<script>
@@ -223,13 +217,15 @@ class Objects
 		}
 	public static function strAudioDeclare()
 		{
-		$str='
+		/*$str='
 		<shader
 			class	="fix brick layer_5"
 			style	="
-				margin-top	:60px;
+				top		:20px;
+				right		:80px;
 				height		:40px;
 				width		:40px;
+				overflow	:hidden;
 				"
 			>
 			<audio 
@@ -245,8 +241,8 @@ class Objects
 					/>
 			</audio>
 		</shader>
-		';
-		return $str;
+		';*/
+		//return $str;
 		}
 	public static function strObjectDeclare()
 		{
