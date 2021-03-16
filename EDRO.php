6ea07dec1989f9d16005a508491081e18879a809
Andrey Chekmarev <assminog@gmail.com>
2021-03-16 09:55:19 +0300

    E D R O v4.789

--- EDRO.php
+++ EDRO.php
@@ -58,21 +58,47 @@ Site[En] Private browsing international: http://ryklzxobxv4s32omimbu7d7t3cdw6dpl
 //                                    |
 //------------------------------------'
 //
-
 set_time_limit(0);
-$мКИМ	= array();
-$оЕДРО	new EDRO($мКИМ, $_strStyle, $arrParams,  $_intLayer);
-require('/home/EDRO.SetOfTools/System/1.Reporter/0.ReportError.php');
-require('/home/EDRO.SetOfTools/System/1.Reporter/1.Report.php');
-require('/home/EDRO.SetOfTools/System/0.Functions/1.FunctionsSetup.php');
-require('/home/EDRO.SetOfTools/System/0.Functions/2.StringFunctions.php');
-require('/home/EDRO.SetOfTools/System/0.Functions/3.Platforms.php');
-require('/home/EDRO.SetOfTools/System/0.Functions/4.Functions.php');
-require('/home/EDRO.SetOfTools/System/0.Functions/5.Dyn.php');
-require('/home/EDRO.SetOfTools/System/2.VectorKIIM/0.KIIM.php');
-//require('/home/EDRO.SetOfTools/System/3.RAM/0.EDRO_Objects.php');
+  require('/home/EDRO.SetOfTools/System/0.Loader/0.loader.php');
+  require('/home/EDRO.SetOfTools/System/1.Reporter/0.ReportError.php');
+  require('/home/EDRO.SetOfTools/System/1.Reporter/1.Report.php');
+  require('/home/EDRO.SetOfTools/System/1.Reporter/2.ReportComplain_Violation_CitizenCountry1_Country2_.php');
+  require('/home/EDRO.SetOfTools/System/1.Reporter/2.ReportComplain_Violation_УКРФ.php');
+  require('/home/EDRO.SetOfTools/System/1.Reporter/2.ReportViloationLaw_ResidenceCountry.php');
+  require('/home/EDRO.SetOfTools/System/1.Reporter/2.ReportViolation_ResourceRules.php');
 
-//EDRO::strHTML($мКИМ, $_strStyle, $_arrParams, $_intLayer);
+//require('/home/EDRO.SetOfTools/System/1.Оповещение/ОтправкаПисьмаАдминистраторуСистемы/ПочтовыйРобот/SendMail/');
+//require('/home/EDRO.SetOfTools/System/1.Оповещение/ОтправкаПисьмаАдминистраторуСистемы/ПочтовыйРобот/SuspectKeys/Complains');
+//require('/home/EDRO.SetOfTools/System/1.Оповещение/ОтправкаПисьмаАдминистраторуСистемы/ПочтовыйРобот/SuspectKeys/Violation/ResidenceCountryLaw');
+//require('/home/EDRO.SetOfTools/System/1.Оповещение/ОтправкаПисьмаАдминистраторуСистемы/ПочтовыйРобот/SuspectKeys/Violation/ResurceRules');
+//require('/home/EDRO.SetOfTools/System/1.Оповещение/ОтправкаПисьмаАдминистраторуСистемы/ПочтовыйРобот/SuspectKeys/Violation/Rights_ResidentCounttry1_residentCountry2');
+require('/home/EDRO.SetOfTools/System/2.Functions/0.key.php');
+require('/home/EDRO.SetOfTools/System/2.Functions/1.FunctionsSetup.php');
+require('/home/EDRO.SetOfTools/System/2.Functions/2.Platforms.php');
+require('/home/EDRO.SetOfTools/System/2.Functions/3.Functions.php');
+require('/home/EDRO.SetOfTools/System/2.Functions/4.Dyn.php');
+require('/home/EDRO.SetOfTools/System/2.Functions/5.StringFunctions.php');
+require('/home/EDRO.SetOfTools/System/2.Functions/5.StringFunctions.php');
+require('/home/EDRO.SetOfTools/System/3.VectorKIIM/0.KIIM.php');
+require('/home/EDRO.SetOfTools/System/3.VectorKIIM/1.objKIIM.activation.php');
+require('/home/EDRO.SetOfTools/System/3.VectorKIIM_Helper/1.objKIIM.activation.php');
+require('/home/EDRO.SetOfTools/System/4.RAM/0.EDRO_Objects.php');
+require('/home/EDRO.SetOfTools/System/5.Styles/0.CSS.Styles.php');
+require('/home/EDRO.SetOfTools/System/6.HTML_Interfaces/0.HTML_HeadInterface.php');
+require('/home/EDRO.SetOfTools/System/7.Templates/0.strKIIM.Template.php');
+
+//We are processing from here.
+//      //
+//     //
+ //   //
+  // //
+   ///
+$мКИМ_0	= array();
+$оЕДРО	=new EDRO($мКИМ, $_strStyle, $arrParams,  $_intLayer);
+
+//require('/home/EDRO.SetOfTools/System/3.RAM/0.EDRO_Objects.php');
+unset($мКИМ_0);
+//EDRO::strHTML($_strStyle, $_arrParams, $_intLayer);
 class EDRO
 	{
 	public 	$strHTML;
@@ -110,7 +136,7 @@ class EDRO
 			'strOpen'		=>'>',
 			'strClose'		=>'<',
 			);
-	public function __construct($мКИМ, $_strStyle, $arrParams,  $_intLayer)
+	public function __construct($_strStyle, $arrParams,  $_intLayer)
 		{
 		//		$this->_КИМ('Start');
 		//		$this->_ПредпусковаяПроверка();
@@ -125,7 +151,7 @@ class EDRO
 /*x1*/		//	$oEDRO		= Event::_V($this->мКИМ, $this->rRadio);
 /*x2*/		//	while($oEDRO->arrEvent['rRadio'])
 		//		{
-				
+
 		//		}
 		//	_Report('Lost radio concrette!!!! AttemptsFromZero: '.$int0Attempts);
 		//	$int0Attempts++;
